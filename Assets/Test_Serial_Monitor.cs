0939d57c49a3b9f2782f7010af52b12f7092e378
RoboBlog <Robovlogg@gmail.com>
2017-03-16 21:56:14 +0100

    serial_test

---
+++ Assets/Test_Serial_Monitor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 	
+using System.IO.Ports;
+
+public class Test : MonoBehaviour {
+
+SerialPort serial = new SerialPort("COM6", 9600);
+
+	void Start () {
+		serial.Open();
+
+		
+	}
+
+	void Update () {
+		  try{
+             print (serial.ReadLine());
+       		}
+        	 catch(System.Exception){
+        	 }
+	}
+}
