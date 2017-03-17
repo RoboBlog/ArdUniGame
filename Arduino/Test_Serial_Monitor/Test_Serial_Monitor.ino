b033ed647b508179410fb63cb4a878bc93a98bb4
RoboBlog <Robovlogg@gmail.com>
2017-03-17 22:45:38 +0100

    Unity_joystick_test

--- Arduino/Test_Serial_Monitor/Test_Serial_Monitor.ino
+++ Arduino/Test_Serial_Monitor/Test_Serial_Monitor.ino
@@ -1,8 +1,41 @@
-void setup() {
-Serial.begin(9600);
+int joystick[6];
+int upbut = 2;
+int rightbut = 3;
+int downbut = 4;
+int leftbut = 5;
+void setup()  
+{
+  Serial.begin(9600);
+ 
+ 
+  pinMode(upbut,INPUT);
+  digitalWrite(upbut,LOW);
+  pinMode(rightbut,INPUT);
+  digitalWrite(upbut,LOW);
+  pinMode(downbut,INPUT);
+  digitalWrite(downbut,LOW);
+  pinMode(leftbut,INPUT);
+  digitalWrite(leftbut,LOW);
+ 
 }
-
-void loop() {
-  Serial.println("TEST");
- Serial.println("TEST2");
-}
+ 
+ 
+void loop()  
+{
+  joystick[2] = digitalRead(upbut);
+  joystick[3] = digitalRead(rightbut);
+  joystick[4] = digitalRead(downbut);
+  joystick[5] = digitalRead(leftbut);
+  
+ 
+    
+      //Serial.print(" Up = "); 
+      Serial.println(digitalRead(upbut));
+      delay(100);
+      //Serial.print(" Right = "); 
+      //Serial.print(digitalRead(rightbut));
+      //Serial.print(" Down = "); 
+     // Serial.print(digitalRead(downbut));
+     // Serial.print(" Left = "); 
+     // Serial.println(digitalRead(leftbut));
+      }
---
+++ Assets/Joystick_Module_Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine; 	
+using System.IO.Ports;
+//using System.Threading;
+
+public class Joystick_Module_Test : MonoBehaviour {
+
+	SerialPort serial = new SerialPort("COM6", 9600);
+	public Rigidbody2D rigi;
+
+	void Start () {
+
+		serial.ReadTimeout = 50;
+		serial.Open();
+		rigi = GetComponent<Rigidbody2D>();
+ 	}
+
+
+	void Update () {
+
+	
+ 		int result = Convert.ToInt32(serial.ReadLine());
+		Debug.Log(result);
+		//Thread.Sleep(100);
+	
+	if(result==0)
+	{
+	
+		//	transform.position = new Vector2(0,0);
+		rigi.velocity = new Vector2(2f *1f, rigi.velocity.y);
+	}
+	else 
+	{
+		//rigi.velocity = new Vector2(-2f *1f, rigi.velocity.y);
+	}
+		
+	}
+}
