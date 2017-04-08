3241d5efcd62e1f039abfb108974d1b487070710
Maciek Dymarczyk <Robovlogg@gmail.com>
2017-04-08 20:35:47 +0200

    v.0.3

--- Assets/Scripts/CameraPlayerFollow.cs
+++ Assets/Scripts/CameraPlayerFollow.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class CameraPlayerFollow : MonoBehaviour {
 
-	private Transform player;
+	private Vector2 velocity;
+	public float smoothTimeX;
+	public float smoothTimeY;
+	
+	public GameObject player;
 	void Start () {
-		player=GameObject.Find("Player").transform;
+		player=GameObject.FindGameObjectWithTag("Player");
 		
 	}
 
-	void Update () {
-		Vector3 playerpos = player.position;
-		playerpos.z = transform.position.z;
-        transform.position = playerpos;
-		
+	void FixedUpdate () {
+		// Vector3 playerpos = player.position;
+		// playerpos.z = transform.position.z;
+        // transform.position = playerpos;
+
+
+		float posX= Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+		float posY= Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+		transform.position = new Vector3(posX, posY,-10);
 	}
 }
--- Assets/Scripts/Controller_Keyboard.cs
+++ Assets/Scripts/Controller_Keyboard.cs
@@ -20,7 +20,7 @@ public class Controller_Keyboard : MonoBehaviour {
 	}
 	
 
-	void Update () {
+	void FixedUpdate () {
 			
 		if(Input.GetKey("d"))
 		{
