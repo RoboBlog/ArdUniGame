717dc91203af6574420010632501507efb000859
Maciek Dymarczyk <Robovlogg@gmail.com>
2017-04-13 14:39:12 +0200

    v0.71

---
+++ Assets/Scripts/HilbertCurve.cs
@@ -0,0 +1,67 @@
+﻿/*Hilbert Curve Implementation
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HilbertCurve : MonoBehaviour {
+
+
+//convert (x,y) to d
+int xy2d(int n, int x, int y)
+{
+    int rx, ry, s, d = 0;
+    for (s = n / 2; s > 0; s /= 2)
+    {
+
+        rx = Convert.ToInt32(((x & s) > 0));
+        ry = Convert.ToInt32((y & s) > 0);
+        d += s * s * ((3 * rx) ^ ry);
+        rot(s, ref x, ref y, rx, ry);
+    }
+    return d;
+}
+
+//convert d to (x,y)
+void d2xy(int n, int d, ref int x, ref int y)
+{
+    int rx, ry, s, t = d;
+    x = y = 0;
+    for (s = 1; s < n; s *= 2)
+    {
+        rx = 1 & (t / 2);
+        ry = 1 & (t ^ rx);
+        rot(s, ref x, ref y, rx, ry);
+        x += s * rx;
+        y += s * ry;
+        t /= 4;
+    }
+}
+
+//rotate/flip a quadrant appropriately
+void rot(int n, ref int  x, ref int y, int rx, int ry)
+{
+    if (ry == 0)
+    {
+        if (rx == 1)
+        {
+            x = n - 1 - x;
+            y = n - 1 - y;
+        }
+
+        //Swap x and y
+        int t = x;
+        x = y;
+        y = t;
+    }
+}
+
+	void Start () {
+
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SimpleEnemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemy: MonoBehaviour {
+
+	public Transform left;
+	public Transform right;
+	public Rigidbody2D mainbody;
+	public bool stop=false;
+	public int move=1;
+	public float speed=0.1f;
+	void Start () {
+		mainbody=GetComponentInParent<Rigidbody2D> ();
+		
+	}	
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	//	Debug.DrawRay(right.position, Vector2.down,Color.red,0.5f);
+		RaycastHit2D hitLeft = Physics2D.Raycast (left.position, Vector2.down, 0.5f);
+		RaycastHit2D hitRight = Physics2D.Raycast (right.position, Vector2.down, 0.5f);
+
+
+		mainbody.MovePosition(mainbody.position+Vector2.left*move*speed);
+		if(hitLeft.collider == null)
+		{
+			stop=true;
+			move=-1;
+		}
+			if(hitRight.collider == null)
+		{
+			stop=true;
+			move=1;
+		}
+	}
+}
